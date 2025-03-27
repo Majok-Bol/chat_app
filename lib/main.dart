0fb14dfaec4089b0efcd9f4acc094a4068f80a53
Majok-Bol <bolmajok001@gmail.com>
2025-03-27 11:00:06 +0300

    added container widget in the Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -14,3 +14,34 @@ class ChatList extends StatefulWidget{
   @override
   ChatListState createState()=>ChatListState();
 }
+class ChatListState extends State<ChatList>{
+  final List<String>chats=[];
+  final TextEditingController chatController=TextEditingController();
+  void addChat(){
+    setState(() {
+      //check that text input field is not empty
+      //if not empty
+      //app text input to chat list
+      if(chatController.text.isNotEmpty){
+        chats.add(chatController.text);
+        chatController.clear();
+      }
+    });
+  }
+  void clearChat(){
+    setState(() {
+      chats.clear();
+    });
+  }
+  @override
+  Widget build(BuildContext){
+    return Scaffold(
+      body: Container(
+        padding:EdgeInsets.all(10),
+        margin: EdgeInsets.all(10),
+        color: Colors.grey,
+      ),
+    );
+
+  }
+}
